5af0c10248be138c46ee86ed110c21c771abaa55
cgeorgea60 <64281418+cgeorgea60@users.noreply.github.com>
2023-02-19 00:39:12 +0100

    cores config file

--- server.js
+++ server.js
@@ -6,8 +6,13 @@ const db = require("./db");
 const app = express();
 
 // Middleware
+const corsOptions = {
+  origin: "https://restaurants-api.up.railway.app",
+  credentials: true,
+  optionSuccessStatus: 200,
+};
 app.use(cors());
-app.use(express.json());
+app.use(express.json(corsOptions));
 
 //Get all restaurants
 app.get("/api/v1/restaurants", async (req, res) => {
